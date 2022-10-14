31e90434899ff6d937a43323bf24ba2dceb6a467
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-10-14 17:47:59 +0200

    ajout doc.txt

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return 'Hello World'
+    return {'message':'Hello World'}
+
+@app.get('/blog/{id}')
+def get_blog(id):
+    return {'message': f'Blog with id {id}'}
