05aa51b309bf110a7f7eb952cbf25304cc682d1f
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2023-03-21 01:01:53 +0100

    fin section 4

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 """
 from typing import Optional
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 from enum import Enum
 from pydantic import BaseModel
 import requests
@@ -18,12 +18,26 @@ def index():
 # def get_all_blogs():
 #     return {'message': 'All blogs provided'}
 
-@app.get('/blog/all')
+@app.get(
+    '/blog/all',
+    tags=['blog'],
+    summary='Retrieve all blogs',
+    description='This api call simulates fetching all blogs',
+    response_description='The list of available blogs'
+    )
 def get_all_blogs(page = 1, page_size: Optional[int] = None):
     return {'message': f'All {page_size} blogs on page {page}'}
 
-@app.get('/blog/{id}/comments/{comment_id}')
+@app.get('/blog/{id}/comments/{comment_id}', tags =['blog', 'comment'])
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulates retrieving a comment of a blog
+
+    - **id** mandatory path parameter
+    - **comment_id** mandatory path parameter
+    - **valid** optional query parameter
+    - **username** optional query parameter
+    """
     return {'message':  f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
 
@@ -32,10 +46,15 @@ class BlogType(str, Enum):
     story ='story'
     howto ='howto'
 
-@app.get('/blog/type/{type}')
+@app.get('/blog/type/{type}', tags=['blog'])
 def get_blog_type(type: BlogType):
     return {'message': f'Blog type {type}'}
 
-@app.get('/blog/{id}')
-def get_blog(id: int):
-    return {'message': f'Blog with id {id}'}
+@app.get('/blog/{id}', status_code=status.HTTP_404_NOT_FOUND, tags=['blog'])
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'Blog {id} not found'}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {'message': f'Blog with id {id}'}
